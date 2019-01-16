a75b4092456dde18af8efd8107fca708e48add06
Tyson Kendon <tj@kendon.ca>
2019-01-16 10:46:58 -0700

    Added 3 example classes to discuss class design

---
+++ class_design/clicker/Clicker.java
@@ -0,0 +1,22 @@
+
+//public interface, what operations do we provide to the outside world
+
+public class Clicker {
+
+    // instance variables
+    // what information do we need to provide our public interface
+
+
+    // constructors
+    // what information do we need to start a clicker
+
+    // accessors
+    // what information do we need to provide to the outside
+
+    // mutators
+    // what changes do we allow the outside world to make on our state
+
+   
+
+
+}
---
+++ class_design/furnace/Furnace.java
@@ -0,0 +1,22 @@
+
+//public interface, what operations do we provide to the outside world
+
+public class Furnace {
+
+    // instance variables
+    // what information do we need to provide our public interface
+
+
+    // constructors
+    // what information do we need to start a furnace
+
+    // accessors
+    // what information do we need to provide to the outside
+
+    // mutators
+    // what changes do we allow the outside world to make on our state
+
+   
+
+
+}
