c145b9aedf9d6747887f82574539c2bc69844476
Tyson Kendon <tj@kendon.ca>
2019-01-16 12:02:54 -0700

    Added design and constructor stubs

--- class_design/clicker/Clicker.java
+++ class_design/clicker/Clicker.java
@@ -1,14 +1,26 @@
 
 //public interface, what operations do we provide to the outside world
 
+// it's a counter, that keeps track of a number (an integer)
+// we can increment the counter and we can reset it to zero
+
 public class Clicker {
 
     // instance variables
     // what information do we need to provide our public interface
+    private int count;
 
 
     // constructors
     // what information do we need to start a clicker
+    public Clicker() {
+
+    }
+
+    public Clicker(int count) {
+	
+    }
+
 
     // accessors
     // what information do we need to provide to the outside
