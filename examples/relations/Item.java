5c676056f49b96b1d9210bb80e2911f3b7cbc8e4
Tyson Kendon <tj@kendon.ca>
2019-02-03 22:14:03 -0700

    Cleaned up formatting

--- examples/relations/Item.java
+++ examples/relations/Item.java
@@ -1,74 +1,67 @@
 
 public class Item {
 
+	private int maxStock;
+	private int currentStock;
+	private String id;
+	private double price;
+	private double totalSales;
 
-
-    private int maxStock;
-    private int currentStock;
-    private String id;
-    private double price;
-    private double totalSales;
-
-    public Item(String id,
-		int maxStock,
-		double price) {
-	this.id = id;
-	this.maxStock = maxStock;
-	this.price = price;
-	this.currentStock = 0;
-	this.totalSales = 0;
-    }
-    
-    public String toString() {
-    	return id + " " + currentStock + " " + price;
-    }
-
-    public String getID() {
-	return this.id;
-    }
-
-    public int getStock() {
-	return this.currentStock;
-    }
-    
-    public double getPrice() {
-	return this.price;
-    }
-
-    public int getMaxStock() {
-	return this.maxStock;
-    }
-
-    public double getTotalSales() {
-
-	return this.totalSales;
-    }
-
-    public boolean restock(int newItems) {
-	boolean returnValue = true;
-	if (currentStock + newItems > maxStock) {
-	    returnValue = false;
-	} else {
-	    this.currentStock += newItems;
+	public Item(String id, int maxStock, double price) {
+		this.id = id;
+		this.maxStock = maxStock;
+		this.price = price;
+		this.currentStock = 0;
+		this.totalSales = 0;
 	}
-	return returnValue;
-    }
 
-    public boolean hasStock() {
-	return currentStock > 0;
-    }
-
-    public boolean buyItem() {
-	if (!hasStock()) {
-	    return false;
+	public String toString() {
+		return id + " " + currentStock + " " + price;
 	}
-	currentStock -= 1;
-	totalSales += price;
-	return true;
 
-    }
-    
+	public String getID() {
+		return this.id;
+	}
 
-    
+	public int getStock() {
+		return this.currentStock;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public int getMaxStock() {
+		return this.maxStock;
+	}
+
+	public double getTotalSales() {
+
+		return this.totalSales;
+	}
+
+	public boolean restock(int newItems) {
+		boolean returnValue = true;
+		if (currentStock + newItems > maxStock) {
+			returnValue = false;
+		} else {
+			this.currentStock += newItems;
+		}
+		return returnValue;
+	}
+
+	public boolean hasStock() {
+		return currentStock > 0;
+	}
+
+	public boolean buyItem() {
+		if (!hasStock()) {
+			return false;
+		}
+		currentStock -= 1;
+		totalSales += price;
+		return true;
+
+	}
 
 }
