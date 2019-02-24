e30e4de656fa8f6d9a6b078c3ab94588c996f349
Tyson Kendon <tj@kendon.ca>
2019-02-24 14:32:10 -0700

    Adding late updates from class

--- examples/class_members/src/ClassMemberExample.java
+++ examples/class_members/src/ClassMemberExample.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
 
 public class ClassMemberExample {
 
-	public static void Main(String[] args) {
+	public static void main(String[] args) {
+		
+		System.out.println(Thing.G);
+		ArrayList<Thing> things = new ArrayList<>();
+		
+		for (int i = 0; i < 5; i++) {
+			things.add(new Thing());
+		}
+		
+		for (Thing t : things) {
+			System.out.println(t);
+		}
 		
 	}
 	
