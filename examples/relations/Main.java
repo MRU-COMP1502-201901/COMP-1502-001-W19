5b1726282d3d04ca39e8296938c708a728dc4226
Tyson Kendon <tj@kendon.ca>
2019-02-24 14:57:23 -0700

    Pulled main from Vending Machine to Main

---
+++ examples/relations/Main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		VendingMachine vending = 
+				new VendingMachine(new Payment());
+		vending.addItem(new Item("A", 10, 1.00));
+		vending.addItem(new Item("A", 10, 1.00));
+		vending.addItem(new Item("B", 10, 1.50));
+		
+		
+		
+		vending.listAllItems();
+		
+		
+		
+		vending.showItemInfo("A");
+		
+		vending.restockItem("A", 5);
+		
+		vending.purchaseItem("A");
+		
+		System.out.println("Total sales: " + vending.getTotalSales());
+		
+		vending.resetSales();
+		
+		System.out.println("Total sales After Reset: " 
+		+ vending.getTotalSales());
+		
+	}
+
+}
