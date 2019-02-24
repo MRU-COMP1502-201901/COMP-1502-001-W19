5ffc7e57261b3dcead315793d7a6ea9e591d438a
Tyson Kendon <tj@kendon.ca>
2019-02-24 15:04:55 -0700

    Made Payment an abstract class, with CreditPayment as a concrete extension

---
+++ examples/relations/vending/CreditPayment.java
@@ -0,0 +1,20 @@
+package vending;
+
+import java.util.Scanner;
+
+public class CreditPayment extends Payment {
+
+	Scanner s = new Scanner(System.in);
+	
+	@Override
+	public double receivePayment() {
+		//
+		double payment = 0.00;
+		
+		System.out.println("Please enter payment");
+		payment = s.nextDouble();
+
+		return payment;
+	}
+
+}
--- examples/relations/vending/Payment.java
+++ examples/relations/vending/Payment.java
@@ -1,19 +1,7 @@
 package vending;
-import java.util.Scanner;
 
-public class Payment {
+public abstract class Payment {
 
-	public double receivePayment() {
-		
-		double payment = 0.00;
-		Scanner s = new Scanner(System.in);
-		System.out.println("Please enter payment");
-		payment = s.nextDouble();
-		
-		return payment;
-		
-		
-		
-	}
+	public abstract double receivePayment();
 	
 }
